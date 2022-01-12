428abd6243f30788237c4ddb1fa7d051cd1e8aee
Duha Emir Ganioglu <emirganioglu@gmail.com>
2022-01-12 23:26:02 +0300

    Part 2 added

---
+++ part2.c
@@ -0,0 +1,236 @@
+/**
+ * virtmem.c 
+ * Authors: Duha Emir Ganioglu, Burak Yildirim
+ * IDs: 71753, 72849
+ */
+
+#include <stdio.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define TLB_SIZE 16
+#define PAGES 1024
+#define PAGE_FRAMES 256
+#define PAGE_MASK 1023 /* TODO */
+
+#define PAGE_SIZE 1024
+#define OFFSET_BITS 10
+#define OFFSET_MASK 1023 /* TODO */
+
+#define MEMORY_SIZE PAGES * PAGE_SIZE
+
+// Max number of characters per line of input file to read.
+#define BUFFER_SIZE 10
+
+struct tlbentry {
+  unsigned char logical;
+  unsigned char physical;
+};
+
+// TLB is kept track of as a circular array, with the oldest element being overwritten once the TLB is full.
+struct tlbentry tlb[TLB_SIZE];
+// number of inserts into TLB that have been completed. Use as tlbindex % TLB_SIZE for the index of the next TLB line to use.
+int tlbindex = 0;
+
+// pagetable[logical_page] is the physical page number for logical page. Value is -1 if that logical page isn't yet in the table.
+int pagetable[PAGES];
+
+signed char main_memory[MEMORY_SIZE];
+
+// Pointer to memory mapped backing file
+signed char *backing;
+
+int max(int a, int b)
+{
+  if (a > b)
+    return a;
+  return b;
+}
+
+int fifoSelector(unsigned char *free_page){
+  //page replacement method with FIFO principle
+	int result = *free_page;
+  // calculating the freepage
+	*free_page = (*free_page + 1) % PAGE_FRAMES;
+	return result;
+}
+
+int lruSelector(int* pageRefTbl, int logical_page, int pageFault) {
+  //page replacement method with LRU principle
+  //it checks whether pagefault is <= frame number
+  if(pageFault <= PAGE_FRAMES) {
+    return pageFault - 1;
+  }
+  int i;
+  int minIdx = 0;
+  int minVal = 0;
+  //it iterates through the pages and finds the minimum index and values
+  for(i = 0; i < PAGES; i++) {
+    if(pagetable[i] != -1) {
+      if(pageRefTbl[i] <= minVal || minVal == 0 ) {
+        minIdx = i;
+        minVal = pageRefTbl[i];
+      }
+    }
+  }
+  //returns the value with the minimum index from pagetable
+  return pagetable[minIdx]; 
+}
+
+// Gets the lru/fifo selection while running the code
+void getInput(int argc, char **argv, int *p) {
+  int opt = 0;
+  while (optind < argc) {
+    while ((opt = getopt(argc, argv, "p:")) != -1) {
+        switch (opt) {
+        case 'p':
+            *p = atoi(optarg);
+            return;
+        default:
+            fprintf(stderr, "Invalid Usage");
+            exit(EXIT_FAILURE);
+        }
+    }
+    optind++;
+  }
+}
+
+void setMemoryWithPage(int physical_page, int logical_page){
+  //adds the page to the memory
+	 memcpy(main_memory + physical_page * PAGE_SIZE, backing + logical_page * PAGE_SIZE, PAGE_SIZE);
+   //iterates through the pages and when ith value of the page table equals to physical page
+   // ith value of pagetable becomes -1 else it equals logical page in the page table to physical page
+   for(int i = 0;i<PAGES;i++){
+     if(pagetable[i] == physical_page){
+	 		pagetable[i] = -1;
+	 	}
+   }
+	 pagetable[logical_page] = physical_page;
+}
+
+/* Returns the physical address from TLB or -1 if not present. */
+int search_tlb(unsigned char logical_page) {
+    /* TODO */
+    int x;
+    //it gets the maximum of 0 and tlbindex - TLB_SIZE as x
+    //then it gets the tlbentry
+    for (x = max(0, (tlbindex - TLB_SIZE)); x < tlbindex; x++) {
+      struct tlbentry *entry = &tlb[x % TLB_SIZE];
+      //returns the physical when logical page equals to the logical entry
+      if (logical_page == (*entry).logical) {
+        return (*entry).physical;
+      }
+    }
+    return -1;
+}
+
+/* Adds the specified mapping to the TLB, replacing the oldest mapping (FIFO replacement). */
+void add_to_tlb(unsigned char logical, unsigned char physical) {
+    /* TODO */
+    //it gets the tlbentry
+    struct tlbentry *entry = &tlb[tlbindex % TLB_SIZE];
+    tlbindex++;
+    //physical and logical of entry setting to the parameters
+    (*entry).physical = physical;
+    (*entry).logical = logical;
+}
+
+int main(int argc, const char *argv[])
+{
+  /*if (argc != 3) {
+    fprintf(stderr, "Usage ./virtmem backingstore input\n");
+    exit(1);
+  }*/
+  
+  int p = 0;
+  getInput(argc, (char**)argv, &p);
+  const char *backing_filename = argv[1]; 
+  int backing_fd = open(backing_filename, O_RDONLY);
+  backing = mmap(0, MEMORY_SIZE, PROT_READ, MAP_PRIVATE, backing_fd, 0); 
+  
+  const char *input_filename = argv[2];
+  FILE *input_fp = fopen(input_filename, "r");
+  
+  // Fill page table entries with -1 for initially empty table.
+  int i;
+  for (i = 0; i < PAGES; i++) {
+    pagetable[i] = -1;
+  }
+  
+  // Character buffer for reading lines of input file.
+  char buffer[BUFFER_SIZE];
+  
+  // Data we need to keep track of to compute stats at end.
+  int total_addresses = 0;
+  int tlb_hits = 0;
+  int page_faults = 0;
+  
+  // Number of the next unallocated physical page in main memory
+  unsigned char free_page = 0;
+  
+  int pageRefTbl[PAGES] = {0};
+  while (fgets(buffer, BUFFER_SIZE, input_fp) != NULL) {
+    total_addresses++;
+    int logical_address = atoi(buffer);
+
+    /* TODO 
+    / Calculate the page offset and logical page number from logical_address */
+    int offset = (logical_address) & (OFFSET_MASK);
+    int logical_page = (logical_address >> OFFSET_BITS) & (PAGE_MASK);
+    ///////
+
+    if(p == 1) { 
+      pageRefTbl[logical_page] = total_addresses;
+    }
+    
+    int physical_page = search_tlb(logical_page);
+    
+    // TLB hit
+    if (physical_page != -1) {
+      tlb_hits++;
+      // TLB miss
+    } else {
+      physical_page = pagetable[logical_page];
+      
+      // Page fault
+      if (physical_page == -1) {
+          /* TODO */
+          page_faults++;
+          // It checks the number of frames to enter into the selection
+          if(free_page<PAGE_FRAMES){
+            physical_page = free_page;
+            free_page++;
+          } else {
+            // The selection between lru and fifo with p as flag
+            if(p == 0){
+              physical_page = fifoSelector(&free_page);
+            } else if (p == 1) {
+              physical_page = lruSelector(pageRefTbl, logical_page, page_faults);
+            }
+          }
+          // it sets the memory with the corresponding page
+          setMemoryWithPage(physical_page, logical_page);
+      }
+      // it adds the page to tlb
+      add_to_tlb(logical_page, physical_page);
+    }
+    
+    int physical_address = (physical_page << OFFSET_BITS) | offset;
+    signed char value = main_memory[physical_page * PAGE_SIZE + offset];
+    
+    printf("Virtual address: %d Physical address: %d Value: %d\n", logical_address, physical_address, value);
+  }
+  
+  printf("Number of Translated Addresses = %d\n", total_addresses);
+  printf("Page Faults = %d\n", page_faults);
+  printf("Page Fault Rate = %.3f\n", page_faults / (1. * total_addresses));
+  printf("TLB Hits = %d\n", tlb_hits);
+  printf("TLB Hit Rate = %.3f\n", tlb_hits / (1. * total_addresses));
+  
+  return 0;
+}
